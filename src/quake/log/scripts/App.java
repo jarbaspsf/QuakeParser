cfeed3427c41311f77531670fe34dc58d63f2ee7
Jarbas Pereira da Silva Filho <jarbas.byakuya@gmail.com>
2013-01-16 15:31:35 -0300

    Removendo Ordenaçao, Task 2 manda somente imprimir o hash

--- src/quake/log/scripts/App.java
+++ src/quake/log/scripts/App.java
@@ -20,13 +20,14 @@ public class App {
 		System.out.println("\nGame:");
 		System.out.println("\nTotal de Mortos: "+p.contagemDeMortos());
 		System.out.println("\nJogadores: "+p.getKillRate().keySet());
-		System.out.println("\nRanking de Kills: ");
+		System.out.println("\nRanking de Kills ordenada em ordem decresente: ");
+		
 		for(String nomeJogador : p.getKillRate().keySet()){
 			System.out.println(nomeJogador+": "+p.getKillRate().get(nomeJogador)+" kills");
 		}
 		
-		System.out.println("\nContagem de meios de Kill:");
-		for(String nomeMeioMorte : p.getMeios_de_morte().keySet()){
+		System.out.println("\nContagem de meios de Kill ordenada em ordem decresente:");
+		for(String nomeMeioMorte : p.getKillRate().keySet()){
 			System.out.println(nomeMeioMorte+": "+p.getMeios_de_morte().get(nomeMeioMorte));
 		}
 		
