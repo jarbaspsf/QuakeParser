f8b846ed95cf47332623aa7d3297485d0baa7db1
Jarbas Pereira da Silva Filho <jarbas.byakuya@gmail.com>
2013-01-16 16:12:05 -0300

    tratando exceções

--- src/quake/log/scripts/App.java
+++ src/quake/log/scripts/App.java
@@ -1,36 +1,45 @@
 package quake.log.scripts;
+
 import java.io.IOException;
 
 import quake.log.parser.Parser;
-
 
 public class App {
 
 	/**
 	 * @param args
-	 * @throws IOException 
+	 * @throws IOException
 	 */
-	public static void main(String[] args) throws IOException {
-		
-		Parser p = new Parser("raw/games.log");
-		
-		System.out.println("...Iniciando Log...");
-		System.out.println("...Atualizando Contagem de Kills...");
-		p.atualizarKills();
-		System.out.println("\nGame:");
-		System.out.println("\nTotal de Mortos: "+p.contagemDeMortos());
-		System.out.println("\nJogadores: "+p.getKillRate().keySet());
-		System.out.println("\nRanking de Kills: ");
-		
-		for(String nomeJogador : p.getKillRate().keySet()){
-			System.out.println(nomeJogador+": "+p.getKillRate().get(nomeJogador)+" kills");
+	public static void main(String[] args) {
+		try {
+			Parser p = new Parser("raw/games.log");
+
+			System.out.println("...Iniciando Log...");
+			System.out.println("...Atualizando Contagem de Kills...");
+			p.atualizarKills();
+			System.out.println("\nGame:");
+			System.out.println("\nTotal de Mortos: " + p.contagemDeMortos());
+			System.out.println("\nJogadores: " + p.getKillRate().keySet());
+			System.out.println("\nRanking de Kills: ");
+
+			for (String nomeJogador : p.getKillRate().keySet()) {
+				System.out.println(nomeJogador + ": "
+						+ p.getKillRate().get(nomeJogador) + " kills");
+			}
+
+			System.out.println("\nContagem de meios de Kill:");
+			for (String nomeMeioMorte : p.getMeios_de_morte().keySet()) {
+				System.out.println(nomeMeioMorte + ": "
+						+ p.getMeios_de_morte().get(nomeMeioMorte));
+			}
+		} catch (IOException e) {
+			System.out.println("Caminho do arquivo incorreto");
+		}catch(ArrayIndexOutOfBoundsException a){
+			System.out.println("Arquivo de log fora do padrão");
+		}catch(Exception e){
+			System.err.println(e);
 		}
-		
-		System.out.println("\nContagem de meios de Kill:");
-		for(String nomeMeioMorte : p.getMeios_de_morte().keySet()){
-			System.out.println(nomeMeioMorte+": "+p.getMeios_de_morte().get(nomeMeioMorte));
-		}
-		
+
 	}
 
 }
